350b9ddce6fc27c3c15c22429bf6618f6953a5a7
Smit Lila <smitlila7@gmail.com>
2024-03-02 16:11:08 -0800

    Update Card.h

--- Card.h
+++ Card.h
@@ -1,13 +1,3 @@
-// // Name: Smit Pravinbhai Lila
-// // Class: CECS 325-02
-// // Project Name (Prog 1 - Fibonacci Solitaire )
-// // Due Date: 09/26/2023
-
-// // I certify that this program is my own original work. I did not copy any part of this program from
-// // any other source. I further certify that I typed each and every line of code in this program.
-
-// attempt 3
-
 #ifndef CARD_H
 
 #define CARD_H
